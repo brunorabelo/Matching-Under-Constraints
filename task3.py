943a51ccf8330f03c4061bbf4ef9f55a8c2e4dfe
Bruno Botelho <brunorabelow@gmail.com>
2022-02-06 00:16:49 +0100

    codigo

---
+++ task3.py
@@ -0,0 +1,90 @@
+def next_unmatched_student(students, matched_students):
+    for student in students:
+        if student not in matched_students:
+            return student
+    return -1
+
+
+def next_school_for_student(matching_table, schools, students, matched_schools, target_student):
+    preferred_schools = matching_table[target_student]
+    for school in preferred_schools:
+        student_group = students[target_student]
+        max_quantity_group_school = schools[school][student_group]
+        current_quantity_group_school = 0
+        for sc, st in matched_schools.items():
+            if str(sc).startswith(school[:-1]) and student_group == students[st]:
+                current_quantity_group_school += 1
+
+        if current_quantity_group_school==max_quantity_group_school and student_group:
+            pass
+
+        if school not in matched_schools:
+            return school
+        current_matched_student = matched_schools[school]
+        preferred_students = matching_table[school]
+
+        for student in preferred_students:
+            if student == target_student:
+                return school
+            if student == current_matched_student:
+                break
+    return -1
+
+
+def deferred_acceptance(matching_table, students, schools):
+    matched_students = {}
+    matched_schools = {}
+    while True:
+        student = next_unmatched_student(students, matched_students)
+        if student == -1:
+            break
+        school = next_school_for_student(matching_table, schools, students, matched_schools, student)
+        if school == -1:
+            break
+        if school in matched_schools:
+            student_unmatched = matched_schools[school]
+            del matched_schools[school]
+            del matched_students[student_unmatched]
+        matched_schools[school] = student
+        matched_students[student] = school
+    return matched_students
+
+
+def get_adjusted_matching_table(matching_table, students, schools):
+    adjusted_table = {}
+    for student in students:
+        row = []
+        for school in matching_table[student]:
+            capacity = schools[school]['capacity']
+            for i in range(0, capacity):
+                new_school_name = school + str(i)
+                row.append(new_school_name)
+                adjusted_table[new_school_name] = matching_table[school]
+        adjusted_table[student] = row
+
+    return adjusted_table
+
+
+def task3():
+    # Instance 1
+    matching_table = {
+        'i1': ['s1', 's2'],
+        'i2': ['s2', 's1'],
+        'i3': ['s1'],
+        'i4': ['s2'],
+        's1': ['i4', 'i3', 'i2', 'i1'],
+        's2': ['i4', 'i3', 'i2', 'i1']
+    }
+    students = {'i1': "A", 'i2': "A", 'i3': "A", 'i4': "B"}
+    schools = {
+        's1': {'quota_group': {"A": 2, "B": 2}, "capacity": 2},
+        's2': {'quota_group': {"A": 2, "B": 2}, "capacity": 2}
+    }
+
+    adjusted = get_adjusted_matching_table(matching_table, students, schools)
+    result1 = deferred_acceptance(adjusted, students, schools)
+
+    # Instance 2
+
+
+task3()
